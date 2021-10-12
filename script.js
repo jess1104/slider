62b7f4571d61cf6133ba3c5dc0ec8d43a93b085f
jess <jesswu@gmail.com>
2021-10-12 16:46:17 +0800

    重包一個外框給圖片，避免重整，避免重整transform影響

--- script.js
+++ script.js
@@ -1,24 +1,36 @@
 // Slider 輪播
 // 先抓到所有的圖
 const slides = document.querySelectorAll(".slide");
+const slidesContainer = document.querySelector(".slide-container");
 const btnLeft = document.querySelector(".slider__btn--left");
 const btnRight = document.querySelector(".slider__btn--right");
 // 抓到點點的容器
 const dotContainer = document.querySelector(".dots");
-// const slider = document.querySelector('.slider');
-// slider.style.transform = 'scale(0.3)';
-// slider.style.overflow = 'visible';
+// const slider = document.querySelector(".slider");
+// slider.style.transform = "scale(0.3)";
+// slider.style.overflow = "visible";
 
 // 這個是要與當下的index去作用
 let curSlide = 0;
 // 當點到底就不能再繼續translate
 const maxSlide = slides.length;
 
+// 切換功能
+const goToSlide = function (slide) {
+  // slidesContainer.style.transform = `translateX(50rem)`;
+  // 所有slides相連
+  // translateX（0%）,100%,200%,300%
+  slides.forEach(function (item, index) {
+    slidesContainer.style.transform = `translateX(${-50 * slide}rem)`;
+  });
+};
+goToSlide(0);
+
 // 創建點點
 const createDots = function () {
-  slides.forEach(function (s, i) {
+  slides.forEach(function (item, index) {
     // 創建點點insertAdjacentHTML
-    dotContainer.insertAdjacentHTML("beforeend", `<button class="dots__dot" data-slide="${i}"></button>`);
+    dotContainer.insertAdjacentHTML("beforeend", `<button class="dots__dot" data-slide="${index}"></button>`);
   });
 };
 createDots();
@@ -31,21 +43,14 @@ const activateDot = function (slide) {
 };
 activateDot(0);
 
-// 切換功能
-const goToSlide = function (slide) {
-  // 所有slides相連
-  // translateX（0%）,100%,200%,300%
-  slides.forEach((s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`));
-};
-goToSlide(0);
-
 // 點擊下一張功能 let curSlide = 0; const maxSlide = slides.length;
 const nextSlide = function () {
   // 與slides長度減一相同時
   if (curSlide === maxSlide - 1) {
-    return;
-    // curSlide = 0;
+    slidesContainer.style.transition = `none`;
+    curSlide = 0;
   } else {
+    slidesContainer.style.transition = `transform .8s`;
     curSlide++;
   }
   goToSlide(curSlide);
@@ -54,11 +59,13 @@ const nextSlide = function () {
 // 點擊上一張功能
 const preslide = function () {
   if (curSlide === 0) {
-    return;
-    // curSide = maxSlide - 1;
+    slidesContainer.style.transition = `none`;
+    curSlide = maxSlide - 1;
   } else {
+    slidesContainer.style.transition = `transform .8s`;
     curSlide--;
   }
+
   goToSlide(curSlide);
   activateDot(curSlide);
 };
@@ -79,7 +86,7 @@ document.addEventListener("keydown", function (e) {
 dotContainer.addEventListener("click", function (e) {
   if (e.target.classList.contains("dots__dot")) {
     // 讓我的slide ＝ 點點相對應的數字
-    console.log(e.target.dataset);
+    // console.log(e.target.dataset);
     const slide = e.target.dataset.slide;
     goToSlide(slide);
     activateDot(slide);
